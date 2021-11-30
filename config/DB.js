b0922655ec020fcd3823137572af6b4bdc4e100b
jackdo1012 <dominhtrijack@gmail.com>
2021-11-30 21:06:35 +0700

    fix db

--- config/DB.js
+++ config/DB.js
@@ -2,11 +2,12 @@ const Pool = require("pg").Pool
 
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
-const pool = new Pool({
-    connectionString:
-        process.env.NODE_ENV === "production"
-            ? process.env.DATABASE_URL
-            : connectionString,
-})
+const pool =
+    process.env.NODE_ENV === "production"
+        ? new Pool({
+              connectionString: process.env.DATABASE_URL,
+              ssl: { rejectUnauthorized: false },
+          })
+        : new Pool({ connectionString })
 
 module.exports = pool
