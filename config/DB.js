570c4230f455173e08d9ed95f64e9284577b9568
jackdo1012 <dominhtrijack@gmail.com>
2023-08-21 18:27:19 +0700

    fix db no pass problem

--- config/DB.js
+++ config/DB.js
@@ -1,6 +1,8 @@
 const Pool = require("pg").Pool;
 
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const connectionString = `postgresql://${process.env.PG_USER}:${
+    process.env.PG_PASSWORD || ""
+}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const pool =
     process.env.NODE_ENV === "production"
@@ -13,7 +15,7 @@ const pool =
 pool.query("SELECT * FROM url_list", (err, _) => {
     if (err) {
         pool.query(
-            `CREATE TABLE url_list ( url_id SERIAL PRIMARY KEY, slug varchar(255), link varchar(255));`
+            `CREATE TABLE url_list ( url_id SERIAL PRIMARY KEY, slug varchar(255), link varchar(255));`,
         );
     }
 });
